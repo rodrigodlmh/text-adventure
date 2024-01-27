013b4d1e6ee2c165b41afca8dcf9513f5d46fd53
rdlmh <84042065+rodrigodlmh@users.noreply.github.com>
2024-01-26 18:30:09 -0600

    Update Main.java

--- text-adventure/src/game/Main.java
+++ text-adventure/src/game/Main.java
@@ -4,10 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("");
-		System.out.print("rodrigo branch");
-		
-		int favoriteNum = 0;
+		try {
+            int result = 10/0;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.err.println("Error: Division by zero is not allowed.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("An unexpected error occurred.");
+            e.printStackTrace();
+        } finally {
+            System.out.println("This block is always executed.");
+        }
 	}
 
 }
