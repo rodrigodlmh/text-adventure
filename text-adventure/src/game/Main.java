768e4a19f2c3067a78b3e9571af094ff29b3d365
Alex-Ehmann <124093868+Alex-Ehmann@users.noreply.github.com>
2024-01-27 15:48:54 -0600

    Update Main.java
    Added a couple of comments

--- text-adventure/src/game/Main.java
+++ text-adventure/src/game/Main.java
@@ -5,12 +5,14 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
 
+	// Logging variable
 	final static Logger log = LogManager.getLogger(Main.class.getName());
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try 
 		{
+			// Attempting to divide
             int result = 10/0;
             System.out.println("Result: " + result);
         } 
