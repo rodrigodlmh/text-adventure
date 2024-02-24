6b3fda4b6838b29a57961ac8c025dd07fb605658
z239290 <157854710+z239290@users.noreply.github.com>
2024-02-24 09:43:23 -0600

    Minor adjustments.

--- text-adventure/src/game/Room.java
+++ text-adventure/src/game/Room.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 @JavaBean
 public class Room implements Serializable {
 
-	private final String roomName;
+	private String roomName;
 
 	private final Item[][]grid = new Item[3][3];
 
@@ -21,7 +21,7 @@ public class Room implements Serializable {
 		return roomName;
 	}
 	
-	public int[][] getGrid()
+	public Item[][] getGrid()
 	{
 		return grid;
 	}
